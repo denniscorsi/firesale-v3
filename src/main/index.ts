0561ef46892b6deedce4c91fb89a9513d3afc275
Dennis Corsi <denniscorsi@gmail.com>
2024-02-18 16:48:15 -0500

    Basic file loading

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,11 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import { join } from 'path';
+import { readFile } from 'fs/promises';
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     webPreferences: {
       preload: join(__dirname, 'preload.js'),
     },
@@ -14,9 +15,15 @@ const createWindow = () => {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(
-      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
     );
   }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    mainWindow.focus();
+    showOpenDialog(mainWindow);
+  });
 
   mainWindow.webContents.openDevTools({
     mode: 'detach',
@@ -36,3 +43,20 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+const showOpenDialog = async (browserWindow: BrowserWindow) => {
+  const result = await dialog.showOpenDialog(browserWindow, {
+    properties: ['openFile'],
+    filters: [{ name: 'Markdown Files', extensions: ['md'] }],
+  });
+
+  if (result.canceled) return;
+  const [filePath] = result.filePaths;
+  console.log(result);
+  openFile(browserWindow, filePath);
+};
+
+const openFile = async (browserWindow: BrowserWindow, filePath: string) => {
+  const content = await readFile(filePath, { encoding: 'utf-8' });
+  console.log(browserWindow, content);
+};
